6c6c3fa42b5cce67dcbf4d036aa84ff7888790f4
NetzZz0 <enzong10@gmail.com>
2024-09-06 20:23:54 -0300

    concertando bugs

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,6 @@ const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin')
 const uglify = require('gulp-uglify')
 
-function scripts() {
-    return gulp.src('./src/scripts/*.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('./dist/js'))
-}
-
 function styles() {
     return gulp.src('./src/styles/*.scss')
         .pipe(sass({ outputStyle: 'compressed' }))
@@ -24,5 +18,4 @@ function images() {
 exports.default = gulp.parallel(styles, images, scripts);
 exports.watch = function () {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
-    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
